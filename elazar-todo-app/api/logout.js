b1a8f2e52b054fccfbc63f7a9df351a67df9fc73
drelazar-gen <drelazar@genesischiropracticrc.com>
2026-08-25 00:23:02 -0400

    Delete elazar-todo-app directory

--- elazar-todo-app/api/logout.js
+++
@@ -1,6 +0,0 @@
-const { clearSessionCookie } = require('../lib/auth');
-
-module.exports = async (req, res) => {
-  res.setHeader('Set-Cookie', clearSessionCookie());
-  res.status(200).json({ ok: true });
-};
